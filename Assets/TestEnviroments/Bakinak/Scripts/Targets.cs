f926d4559e0c8f9751025881c0e5a14b0709e8df
Bakinak <akobbe16@student.aau.dk>
2021-03-27 22:27:48 +0100

    Targets can fade in and out, forcing players to change targets.

--- Assets/TestEnviroments/Bakinak/Scripts/Targets.cs
+++ Assets/TestEnviroments/Bakinak/Scripts/Targets.cs
@@ -13,10 +13,18 @@ public class Targets : SoundClass
     public AudioClip mySound;
     private Monsterspawner monsterspawner;
     // Start is called before the first frame update
+
+    public int startHitpoint = 10;
+    int currentHitpoint;
+    public float respawnTime = 15;
+    public float fadeSpeed = 5;
+    private bool fading = true;
+
     void Start()
     {
         myParticles = GetComponent<ParticleSystem>();
         monsterspawner = GameObject.FindObjectOfType<Monsterspawner>().GetComponent<Monsterspawner>();
+        currentHitpoint = startHitpoint;
     }
 
 
@@ -28,8 +36,8 @@ public class Targets : SoundClass
         playSound(mySound);
         Destroy(creature.transform.parent.gameObject);
         monsterspawner.monsterSpawned--;
-        
 
+        updatehitpoint();
     }
 
     
@@ -52,7 +60,65 @@ public class Targets : SoundClass
         }
     }
 
+    void updatehitpoint()
+    {
+        currentHitpoint -= 1;
+
+        if(currentHitpoint <= 0)
+        {
+            if (fading == true)
+            {
+                fading = false;
+                StartCoroutine(fadeOut());
+            }
+        }
+    }
 
 
+    IEnumerator fadeOut()
+    {
+        while(this.GetComponent<SpriteRenderer>().color.a > 0)
+        {
+            Color objectColor = this.GetComponent<SpriteRenderer>().color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.GetComponent<SpriteRenderer>().color = objectColor;
+            if (this.GetComponent<SpriteRenderer>().color.a <= 0)
+            {
+                GetComponent<BoxCollider2D>().enabled = false;
+                GetComponent<SpriteRenderer>().enabled = false;
+                StartCoroutine(ExecuteAfterTime(respawnTime));
+            }
+            yield return null;
+        }
+
+    }
+
+
+    IEnumerator fadeIn()
+    {
+        while (this.GetComponent<SpriteRenderer>().color.a < 1)
+        {
+            Color objectColor = this.GetComponent<SpriteRenderer>().color;
+            float fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.GetComponent<SpriteRenderer>().color = objectColor;
+            yield return null;
+        }
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        StartCoroutine(fadeIn());
+        // Code to execute after the delay
+        currentHitpoint = startHitpoint;
+        GetComponent<BoxCollider2D>().enabled = true;
+        GetComponent<SpriteRenderer>().enabled = true;
+        fading = true;
+
+    }
 
 }
