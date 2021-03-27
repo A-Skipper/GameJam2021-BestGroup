bbc4733d0305f419dc775336454ec91312bc501f
Skippzzz <anders.skipper@outlook.dk>
2021-03-27 21:55:49 +0100

    Better handle of destroy objects

--- Assets/TestEnviroments/Skippzzz/Kill.cs
+++ Assets/TestEnviroments/Skippzzz/Kill.cs
@@ -11,24 +11,10 @@ public class Kill : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        try
+        if (other.transform.tag == "creature")
         {
-            //Either center or outer colider of object can hit
-            if (other.gameObject.transform.parent.gameObject.transform.parent.gameObject != null)
-            {
-                Destroy(other.gameObject.transform.parent.gameObject.transform.parent.gameObject);
-                monsterspawner.monsterSpawned--;
-            }
-            else
-            {
-                Destroy(other.gameObject.transform.parent.gameObject);
-                monsterspawner.monsterSpawned--;
-            }
-        }
-        catch (NullReferenceException)
-        {
-            //Nothing need to happen if the gameobject is already destoryed
-            //Debug.Log("Object already destroyed");
+            Destroy(other.transform.parent.gameObject);
+            monsterspawner.monsterSpawned--;
         }
         
     }
