6740b0d6c14abeb7e95f48e31c961826c86515e9
Skippzzz <anders.skipper@outlook.dk>
2021-03-27 11:29:02 +0100

    Added loading screen with progress bar between scenes

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public static class SceneLoader
 {
+    private class LoadingMono : MonoBehaviour { }
+
     public enum Scene
     {
         MainMenu,
@@ -11,17 +15,45 @@ public static class SceneLoader
     }
 
     private static Action onLoaderCallback;
+    private static AsyncOperation loadingAsyncOperation;
 
     public static void Load(Scene scene)
     {
         //Set loader callback to load target scene
         onLoaderCallback = () =>
         {
-            SceneManager.LoadScene(scene.ToString());
+            GameObject loadingGameObject = new GameObject("Loading game object");
+            loadingGameObject.AddComponent<LoadingMono>().StartCoroutine(LoadSceneAsync(scene));
+            
         };
         //Load loading scene
         SceneManager.LoadScene(Scene.Loading.ToString());
     }
+
+    private static IEnumerator LoadSceneAsync(Scene scene)
+    {
+        yield return null;
+
+        loadingAsyncOperation = SceneManager.LoadSceneAsync(scene.ToString());
+
+        while (!loadingAsyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public static float GetLoadingProgress()
+    {
+        if (loadingAsyncOperation != null)
+        {
+            return loadingAsyncOperation.progress;
+        }
+        else
+        {
+            return 1f;
+        }
+    }
+
 
     public static void LoaderCallback()
     {
